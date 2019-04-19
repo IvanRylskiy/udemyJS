2f8f38fa906c97fed5fe3be12ea7b784a730e4a6
Ivan <ivan.rylskiy@gmail.com>
2019-04-19 19:52:16 +0700

    First exercise edited

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var money,
-    time,
-    expensesWhat,
-    expensesHowMuch,
-    dailyMoney;
+let money,
+    time;
 
 money = +prompt('Ваш бюджет на месяц?', '');
 time = prompt('Введите дату в формате YYYY-MM-DD', '');
-expensesWhat = prompt('Введите обязательную статью расходов в этом месяце', '');
-expensesHowMuch = +prompt('Во сколько обойдется?', '');
+
+let expensesWhat1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    expensesHowMuch1 = +prompt('Во сколько обойдется?', ''),
+    expensesWhat2 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    expensesHowMuch2 = +prompt('Во сколько обойдется?', '');
 
 let appData = {
     moneyData: money,
     timeData: time,
     expenses: {
-        [expensesWhat]: expensesHowMuch
+        [expensesWhat1]: expensesHowMuch1,
+        [expensesWhat2]: expensesHowMuch2
     },
-    optionalExpenses: undefined,
-    income: undefined,
+    optionalExpenses: {},
+    income: [],
     savings: false
 };
 
-dailyMoney = (money - expensesHowMuch) / 30;
-
-alert('Бюджет на 1 день: ' + dailyMoney);
+console.log(appData);
+alert(appData.moneyData / 30);
