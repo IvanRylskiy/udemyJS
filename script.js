94e81d273527172e7f222188ed0ccebe508e9be5
Ivan <ivan.rylskiy@gmail.com>
2019-04-19 18:26:12 +0700

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone);
+
+let array = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(array);
+
+// alert('Hello world!');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = prompt('Are you here?', '');
+
+// console.log(answer);
+// console.log(typeof(array));
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
