f85149c90b8a2d61199bbff21dd001cb42093257
Ivan <ivan.rylskiy@gmail.com>
2019-04-20 20:15:30 +0700

    creating app.js file for app lessons

--- script.js
+++ script.js
@@ -44,3 +44,26 @@ for (let i = 1; i < 8; i++) {
     }
     console.log(i);
 }
+
+//Задачи на понимание основ JS
+// console.log([] + false - null + true);
+
+// let y = 1; 
+// let x = y = 2; 
+// console.log(x);
+
+// console.log([] + 1 + 2);
+
+// console.log("1"[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// let a = 1, b = 1;
+// console.log(!!( a && b ));
+// console.log(a && b);
+
+// console.log(null || 2 && 3 || 4);
+
+// console.log(+"Infinity");
+
+// console.log(0 || "" || 2 || undefined || true || false);
