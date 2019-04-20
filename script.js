0fe58d533e9a1cb7f0e07a44e787a1184ead175a
Ivan <ivan.rylskiy@gmail.com>
2019-04-20 19:43:11 +0700

    cycle lesson

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ switch (num) {
         console.log('Something went wrong!');
         break;
 }
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
