da0b4c239e1d8d0668c72686e74d0c15437be0b6
Ivan <ivan.rylskiy@gmail.com>
2019-04-21 19:28:43 +0700

    objects lesson

--- script.js
+++ script.js
@@ -126,3 +126,30 @@ function done () {
 }
 
 learnJS('JavaScript', done);
+
+//OBJECTS
+
+let options = {
+    width: 1024,
+    height: 1024,
+    name: 'Name'
+};
+
+console.log(options.name);
+
+options.bool = false;
+options.colors = {
+    border: 'black',
+    bg: 'red'
+};
+
+delete options.bool;
+
+console.log(options);
+
+for (let key in options) {
+    console.log('Свойство ' + key + ' имеет значение ' 
+    + options[key]);
+}
+
+console.log(Object.keys(options).length);
