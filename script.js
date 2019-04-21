ab0dafa9729ca0dd30b9c61e659ac4c9119c79cc
Ivan <ivan.rylskiy@gmail.com>
2019-04-21 14:07:09 +0700

    function lesson

--- script.js
+++ script.js
@@ -44,3 +44,50 @@ switch (num) {
 //     }
 //     console.log(i);
 // }
+
+// function showFirstMessage (text) {
+//     alert(text);
+//     let num = 20;
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello!');
+// console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(3, 4));
+
+console.log(calc(3, 7));
+
+function retVar() {
+    let some = 50;
+    return some;
+}
+
+let anotherSome = retVar();
+
+console.log(anotherSome);
+
+let stuff = function(a, b) {
+    return (a + b);
+};
+
+console.log(stuff(3, 5));
+
+let anotherCalc = (j, c) => j + c;
+
+console.log(anotherCalc(3, 5));
+
+let str = 'text';
+
+console.log(str.length);
+console.log(str.toUpperCase());
+
+let twelve = '12.2px';
+
+// console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
