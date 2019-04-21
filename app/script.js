ba4b1ac890c705833db9d272fdd911a45525f3dd
Ivan <ivan.rylskiy@gmail.com>
2019-04-21 16:26:55 +0700

    re-doin' all stuff with functions, ex.4

--- app/script.js
+++ app/script.js
@@ -1,8 +1,15 @@
-let money,
-    time;
+let money, time;
 
-money = +prompt('Ваш бюджет на месяц?', '');
-time = prompt('Введите дату в формате YYYY-MM-DD', '');
+function start () {
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+}
+
+start();
 
 let appData = {
     moneyData: money,
@@ -10,36 +17,78 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-    let expensesWhat = 
-    prompt('Введите обязательную статью расходов в этом месяце', ''),
-        expensesHowMuch = +prompt('Во сколько обойдется?', '');
-
-    if ( (typeof(expensesWhat)) === 'string' && (typeof(expensesWhat)) != null 
-        && (typeof(expensesHowMuch)) != null && expensesWhat != ''
-        && expensesHowMuch != '' && expensesWhat.length < 50 ) {
-        appData.expenses[expensesWhat] = expensesHowMuch;
-        console.log('Done!');
-    } else {
-        expensesWhat = 
-        prompt('Введите обязательную статью расходов в этом месяце', '');
-        expensesHowMuch = +prompt('Во сколько обойдется?', '');
+function chooseExpenses () {
+    for (let i = 0; i < 2; i++) {
+        let expensesWhat = 
+        prompt('Введите обязательную статью расходов в этом месяце', ''),
+            expensesHowMuch = +prompt('Во сколько обойдется?', '');
+    
+        if ( (typeof(expensesWhat)) === 'string' 
+            && (typeof(expensesWhat)) != null 
+            && (typeof(expensesHowMuch)) != null && expensesWhat != ''
+            && expensesHowMuch != '' && expensesWhat.length < 50 ) {
+            appData.expenses[expensesWhat] = expensesHowMuch;
+            console.log('Done!');
+        } else {
+            expensesWhat = 
+            prompt('Введите обязательную статью расходов в этом месяце', '');
+            expensesHowMuch = +prompt('Во сколько обойдется?', '');
+        }
     }
 }
 
-appData.moneyPerDay = appData.moneyData / 30;
+chooseExpenses();
 
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+function detectDayBudget () {
+    appData.moneyPerDay = +((appData.moneyData / 30).toFixed()); // (1) для 1 зн
 
-if (appData.moneyPerDay < 100) {
-    console.log('You are gay!');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('You are regular guy!');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Wow, you are such a handsome boi! Fuck me!');
-} else {
-    console.log ('Die pls!');
+    alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 }
+
+detectDayBudget();
+
+function detectLevel () {
+    if (appData.moneyPerDay < 100) {
+        console.log('You are gay!');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('You are regular guy!');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('Wow, you are such a handsome boi! Fuck me!');
+    } else {
+        console.log ('Die pls!');
+    }
+}
+
+detectLevel();
+
+function checkSavings () {
+    if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений?', ''),
+            percent = +prompt('Под какой процент?', '');
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        
+        alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses () {
+    for (let i = 1; i < 4; i++) {
+        let optExpensesWhat = prompt('Статья необязательных расходов?', '');
+        while (optExpensesWhat == '' || optExpensesWhat == null 
+            || typeof(optExpensesWhat) != 'string') {
+            optExpensesWhat = prompt('Статья необязательных расходов?', '');
+        }
+
+        appData.optionalExpenses[i] = optExpensesWhat;
+    }
+}
+
+chooseOptExpenses();
+
+console.log(appData);
