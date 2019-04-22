e8690585a88242d945b5690c0ab815e8f5389a8a
Ivan <ivan.rylskiy@gmail.com>
2019-04-22 17:34:09 +0700

    OOP lesson

--- script.js
+++ script.js
@@ -200,3 +200,21 @@ function compareNum(a, b) {
 
 arrC = arrB.sort(compareNum);
 console.log(arrC);
+
+//Object-oriented programming
+
+//Объект - самая главная сущность!
+
+let soldier = {
+    health: 400,
+    armor: 100
+};
+
+let john = {
+    health: 100
+};
+
+john.__proto__ = soldier;
+
+console.log(john);
+console.log(john.armor);
