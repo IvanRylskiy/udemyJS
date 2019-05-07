3d4361e594553d59e1a74b547f1944b63518917b
Ivan <ivan.rylskiy@gmail.com>
2019-05-07 15:54:26 +0700

    Timeout & Interval lesson

--- JS_in_work/script.js
+++ JS_in_work/script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+//Timeout & Interval
+
+// setTimeout(func, delay);
+
+// function sayHello() {
+//     console.log('Hello');
+// }
+
+// let timerId = setTimeout(sayHello, 3000);
+// clearTimeout(timerId);
+
+// let timerId = setInterval(sayHello, 3000);
+// clearInterval(timerId);
+
+// рекурсивный вызов setTimeout
+
+// let timerId = setTimeout(function log() {
+//     console.log('Hello');
+//     setTimeout(log, 2000);
+// });
+
+let btn = document.querySelector('.btn'),
+    elem = document.querySelector('.elem');
+
+function myAnimation () {
+    let pos = 0;
+
+    let id = setInterval(frame, 10);
+    function frame () {
+        if (pos == 200) {
+            clearInterval();
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// Делегирование событий
+
+let btnBlock = document.querySelector('.btn-block'),
+    btns = document.getElementsByTagName('button');
+
+btnBlock.addEventListener('click', function(event) {
+    if (event.target && event.target.matches('button.first')) {
+        console.log('Wassup');
+    }
+});
+
+// event.target.tagName == 'BUTTON'
+// event.target.classList.contains('first')
