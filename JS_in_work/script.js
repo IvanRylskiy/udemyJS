e34e3562d200bddd512f96676303aeb97c6d1a73
Ivan <ivan.rylskiy@gmail.com>
2019-05-15 18:40:57 +0700

    this lesson

--- JS_in_work/script.js
+++ JS_in_work/script.js
@@ -128,3 +128,68 @@ console.log(ivanes6);
 console.log(alexes6);
 
 ivanes6.exit();
+
+//This - контекст вызова
+
+function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+    console.log(sum());
+}
+
+showThis(4, 5);
+showThis(5, 5);
+
+let obj = {
+    a: 20,
+    b: 15,
+    sum: function() {
+        console.log(this);
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+};
+
+obj.sum();
+
+let user = {
+    name: 'John'
+};
+
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + ' ' + surname);
+}
+
+console.log(sayName.call(user, 'Doe'));
+console.log(sayName.apply(user, ['Snow'])); // несколько аргументов
+
+function count(number) {
+    return this * number;
+}
+
+let double = count.bind(2);
+
+console.log(double(3));
+console.log(double(10));
+
+let btnThis = document.querySelector('.btn-this');
+
+btnThis.addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis() {
+        console.log(this);
+    }
+    showThis();
+});
+
+// 1) Просто вызов функции - window/undefind
+// 2) Метод объекта - this = объект
+// 3) Конструктор (new) - this = новый созданный объект
+// 4) Указание конкретного контекста - call, apply, bind
